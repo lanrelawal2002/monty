5211f9514f2d72e39add5f1ffe4cdaf3c1afdb34
Odsyesufu7 <syesufu2000@yahoo.com>
2022-07-14 09:47:52 +0000

    Collaborative task

---
+++ interpreter_cleanup.c
@@ -0,0 +1,23 @@
+
+#include "monty.h"
+
+/**
+ * free_stack - frees the stack on exit
+ *
+ * @stack: pointer to the stack
+ *
+ * Return: void
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *current = NULL;
+
+	current = stack;
+
+	if (current != NULL)
+	{
+		free_stack(current->next);
+		free(current);
+	}
+}
+
