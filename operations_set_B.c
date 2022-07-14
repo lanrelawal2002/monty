f22f4d393b5283519d8c7bdc10002794ec6de2ca
OLUWASEYI DAUDA YESUFU <102301571+Odsyesufu7@users.noreply.github.com>
2022-07-14 22:07:58 +0100

    Collaborative task

---
+++ operations_set_B.c
@@ -0,0 +1,55 @@
+#include "monty.h"
+
+/**
+ * _swap - swaps the top two elements of the stack.
+ * @stack: Stack
+ * @line_number: Number of the line
+ */
+void _swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	int temp_val = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp_val = current->n;
+	current->n = current->next->n;
+	current->next->n = temp_val;
+}
+
+/**
+ * m_add - add the top two elements of the stack
+ * @stack: double pointer tot he beginning of the stack
+ * @line_number: script line number
+ *
+ * Return: void
+ */
+void _add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = NULL;
+	int sum = 0;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	current = *stack;
+	sum = current->n + current->next->n;
+	current->next->n = sum;
+	_pop(stack, line_number);
+}
+
+/**
+ * _nop - function void
+ * @stack: Stack list
+ * @line_number: Number of the line
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
